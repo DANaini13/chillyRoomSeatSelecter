08a302d4f2cea15d7241bc09117f8efece6c3314
enchanterpang <859616130@qq.com>
2020-06-16 22:40:59 +0800

    UNITY第一个版本提交

---
+++ SeatSelecter/Assets/Scripts/SeatEventControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class SeatEventControl : MonoBehaviour
+{
+    public GameObject canvasObj;
+    public GameObject sceneObj;
+
+    public Text errorText;
+
+    public InputField InputField;
+
+    public void CheckName()
+    {
+        //检查姓名是否通过
+        if (InputField.text =="xjj")
+        {
+            SeatManager.Instance.curName = "xjj";
+            canvasObj.SetActive(false);
+            sceneObj.SetActive(true);
+        }
+        else
+        {
+            errorText.DOKill();
+            errorText.color = new Color32(255, 37, 37, 255);
+
+            errorText.DOColor(new Color32(255, 37, 37, 0), 1).SetDelay(1);
+        }
+    }
+}
---
+++ SeatSelecter/Assets/Scripts/SeatManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeatManager : BaseSingleton<SeatManager>
+{
+    public Transform seatParent;
+    public List<SeatInfo> seatInfos;
+    public GameObject playerObj;
+    // Start is called before the first frame update
+
+    public string curName ;
+
+    private void Awake()
+    {
+        seatInfos = new List<SeatInfo>();
+    }
+    void Start()
+    {
+        for (int i = 0; i < seatParent.childCount; i++)
+        {
+            SeatInfo seatInfo = seatParent.GetChild(i).GetComponent<SeatInfo>();
+            if (seatInfo != null)
+            {
+                seatInfos.Add(seatInfo);
+            }
+
+
+        }
+    }
+
+
+    public void RefreshPlayer()
+    {
+        while (seatInfos.Count ==0)
+        {
+            return;
+        }
+        Debug.Log("刷新");
+        for (int i = 0; i < seatInfos.Count; i++)
+        {
+            if (!seatInfos[i].isNull)
+            {
+                seatInfos[i].playerObj = Instantiate(playerObj, seatInfos[i].transform);
+                seatInfos[i].text.text = curName;
+            }
+            else
+            {
+                if (seatInfos[i].playerObj != null)
+                {
+                    Destroy(seatInfos[i].playerObj);
+                }
+                seatInfos[i].text.text = "空";
+            }
+        }
+    }
+
+}
